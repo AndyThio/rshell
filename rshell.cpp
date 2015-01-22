f9e822f575b1e01b6b5cca151d65332bdef404e4
AndyThio <andypthio@gmail.com>
2015-01-22 01:26:54 -0800

    Added rshell.cpp

---
+++ rshell.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <unistd.h>
+#include <cstring>
+int main(){
+    while(true){
+        char **uin;
+        cout << "$ ";
+        cin << uin;
+    }
+    return 0;
+}
